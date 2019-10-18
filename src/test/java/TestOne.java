2e1614324273d9734594c3b0ec42887c037f38df
vmejinskii <vladimir.mejinskii@zensoft.io>
2019-10-18 20:14:34 +0600

    Update chrome webdriver Add methods for test google search

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -2,9 +2,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class TestOne {
 
@@ -12,13 +16,19 @@ public class TestOne {
 
     @Before
     public void before() {
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
+        WebDriverManager.chromedriver().version("77.0").setup();
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1820,900));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://google.com/");
     }
 
     @Test
-    public void test_01() {
-        driver.get("http://yandex.ru/");
+    public void test_01_Search() {
+        driver.findElement(By.cssSelector(".gLFyf.gsfi")).sendKeys("CSS vs XPATH locators");
+        driver.findElement(By.cssSelector(".gNO89b")).click();
+
+
     }
 
     @After
