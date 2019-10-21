82428f572471de61a41285ab7b28f6cb555573dc
vmejinskii <vladimir.mejinskii@zensoft.io>
2019-10-21 20:27:44 +0600

    Tests for google search

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -8,10 +8,11 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertArrayEquals;
 
 public class TestOne {
 
@@ -23,32 +24,19 @@ public class TestOne {
         driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1820, 900));
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.google.com");
+        driver.get("https://www.google.com/?hl=ru");
     }
 
     @Test
     public void test_01_Search() {
         driver.findElement(By.cssSelector(".gLFyf.gsfi")).sendKeys("CSS vs XPATH locators");
         driver.findElement(By.cssSelector(".gNO89b")).click();
-        //System.out.println(driver.findElements(By.cssSelector("h3[class='LC20lb']")).size());
-
-        //Check rows on google search result
-        int count = 0;
-        List<WebElement> rows = driver.findElements(By.cssSelector("h3[class='LC20lb']"));
-        for (WebElement row : rows) {
-            if (row.isDisplayed())
-                count++;
-        }
-        System.out.println("Number of rows: " + count);
-
+        List<WebElement> rows = driver.findElements(By.xpath("//div/a/div/img[@data-atf]"));
+        System.out.println("Number of rows: " + rows.size());
+        assertArrayEquals(new int[]{11}, new int[]{rows.size()});
         driver.findElements(By.cssSelector("h3[class='LC20lb']")).get(0).click();
-
-        //Check text on the page
-        if (driver.getPageSource().contains("Locating Element Using XPATH Selectors")) {
-            System.out.println("Text is displayed on the page");
-        } else {
-            System.out.println("Fail");
-        }
+        String bodyText = driver.findElement(By.tagName("body")).getText();
+        Assert.assertTrue("Text not found!", bodyText.contains("Locating Element Using XPATH Selectors"));
     }
 
     @After
